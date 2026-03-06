5b67ac632df44141b04a02af3c7424bf5b0b608b
mathewbranch16 <mathewbranch16@gmail.com>
2026-03-06 23:37:00 +0530

    Fix footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,9 @@ function Footer() {
       <div className="container">
         <p>&copy; 2024 The Plant Project. All rights reserved.</p>
         <div className="social-icons">
-          <a href="https://www.facebook.com/branch.mathew.5" target="_blank"><i className='bx bxl-facebook'></i></a>
-          <a href="https://www.instagram.com/mathewbranch16/" target="_blank"><i className='bx bxl-instagram'></i></a>
-          <a href="https://x.com/branchudi" target="_blank"><i className='bx bxl-twitter'></i></a>
+          <a href="https://www.facebook.com/branch.mathew.5" target="_blank" rel="noreferrer"><i className='bx bxl-facebook'></i></a>
+          <a href="https://www.instagram.com/mathewbranch16/" target="_blank" rel="noreferrer"><i className='bx bxl-instagram'></i></a>
+          <a href="https://x.com/branchudi" target="_blank" rel="noreferrer"><i className='bx bxl-twitter'></i></a>
         </div>
       </div>
     </footer>
@@ -16,4 +16,3 @@ function Footer() {
 }
 
 export default Footer;
-
